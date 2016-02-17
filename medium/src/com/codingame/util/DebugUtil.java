a6c92f56fc7ad818b17d230afa588c4416397f16
mark.hennessy <mhennessy116@gmail.com>
2016-02-17 11:25:02 +0100

    Added example code

---
+++ medium/src/com/codingame/util/DebugUtil.java
@@ -0,0 +1,35 @@
+package com.codingame.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DebugUtil {
+
+    public static void d(String label, Object obj) {
+        String string = obj.toString();
+        System.err.println(label + ": " + string);
+    }
+
+    public static void d(String label, int[] intArray) {
+        String string = Arrays.stream(intArray)
+                .mapToObj(Integer::toString)
+                .collect(Collectors.joining(", "));
+        System.err.println(label + ": " + string);
+    }
+
+    public static <T> void d(String label, T[] array) {
+        String string = Arrays.stream(array)
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
+        System.err.println(label + ": " + string);
+    }
+
+    public static <T> void d(String label, List<T> list) {
+        String string = list.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(", "));
+        System.err.println(label + ": " + string);
+    }
+
+}
--- sandbox/src/com/codingame/Main.java
+++ sandbox/src/com/codingame/Main.java
@@ -1,8 +1,26 @@
 package com.codingame;
 
+import java.util.Comparator;
+import java.util.function.Function;
+
 public class Main {
     
     public static void main(String[] args) {
+        // This only works because Comparator is a @FunctionalInterface
+        Comparator<Integer> comparator = (a, b) -> a < b ? -1 : 1;
+        Comparator<Integer> comparator2 = (a, b) -> { 
+            if (a < b) {
+                return -1;
+            }
+            else if (a == b) {
+                return 0;
+            } 
+            else {
+                return 1;
+            }
+        };
         
+        Function<Object, String> func = Object::toString;
+        String five = func.apply(5);
     }
 }
